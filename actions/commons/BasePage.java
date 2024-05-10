2e672a1fa29cfa1b1ec745d8933df66a9ffa48ec
admin <jamesnguyen0102@gmail.com>
2024-05-10 17:27:33 +0700

    Build BasePage class - Part IV

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -3,93 +3,154 @@ package commons;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Set;
 
 public class BasePage {
 
-    WebDriver driver;
-    WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
-
-    public void getUrl(String url) {
+    public void openPageUrl(WebDriver driver, String url) {
         driver.get(url);
     }
 
-    public String getTitle() {
+    public String getTitle(WebDriver driver) {
         return driver.getTitle();
     }
 
-    public String getCurrentURL() {
+    public String getPageURL(WebDriver driver) {
         return driver.getCurrentUrl();
     }
 
-    public void back() {
+    public String getPageSourceCode(WebDriver driver) {
+        return driver.getPageSource();
+    }
+
+    public void backToPage(WebDriver driver) {
         driver.navigate().back();
     }
 
-    public void forward() {
+    public void forwardToPage(WebDriver driver) {
         driver.navigate().forward();
     }
 
-    public void refresh() {
+    public void refreshCurrentPage(WebDriver driver) {
         driver.navigate().refresh();
     }
 
     //Alert
-    public void waitAlertPresence() {
-        explicitWait.until(ExpectedConditions.alertIsPresent());
-
+    public Alert waitAlertPresence(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.alertIsPresent());
     }
 
-    public void cancelAlert(Alert alert) {
-        alert.dismiss();
+    public void cancelAlert(WebDriver driver) {
+        waitAlertPresence(driver).dismiss();
     }
 
-    public String getTextAlert(Alert alert) {
-        return alert.getText();
+    public String getTextAlert(WebDriver driver) {
+        return waitAlertPresence(driver).getText();
     }
 
-    public void sendKeyToAlert(Alert alert, String sendText) {
-        alert.sendKeys(sendText);
+    public void sendKeyToAlert(WebDriver driver, String sendText) {
+        waitAlertPresence(driver).sendKeys(sendText);
     }
 
     //Windows
 
-    public String getWindowHandle(){
+    public String getWindowHandle(WebDriver driver) {
         return driver.getWindowHandle();
     }
 
-    public void switchWindowByID(String idCurrentPage){
+    public void switchWindowByID(WebDriver driver, String idCurrentPage) {
         Set<String> allIDs = driver.getWindowHandles();
-        for (String id : allIDs){
-            if (id.equals(idCurrentPage)){
+        for (String id : allIDs) {
+            if (!id.equals(idCurrentPage)) {
                 driver.switchTo().window(id);
                 break;
             }
         }
     }
 
-    public void switchWindowByTitle(String titlePage){
+    public void switchWindowByTitle(WebDriver driver, String titlePage) {
         Set<String> allIDs = driver.getWindowHandles();
-        for (String id : allIDs){
+        for (String id : allIDs) {
             driver.switchTo().window(id);
-            if (driver.getTitle().equals(titlePage)){
+            if (driver.getTitle().equals(titlePage)) {
                 break;
             }
 
         }
     }
 
-    public void closeAllWindowsWithoutParent(String parentID){
+    public void closeAllWindowsWithoutParent(WebDriver driver, String parentID) {
         Set<String> allIDs = driver.getWindowHandles();
-        for (String id : allIDs){
-            if (!id.equals(parentID)){
+        for (String id : allIDs) {
+            if (!id.equals(parentID)) {
                 driver.switchTo().window(id);
                 driver.close();
             }
+        }
+        driver.switchTo().window(parentID);
+    }
+
+    public WebElement getElement(WebDriver driver, String locator) {
+        return driver.findElement(getByXpath(locator));
+    }
+
+    public List<WebElement> getElements(WebDriver driver, String locator) {
+        return driver.findElements(getByXpath(locator));
+    }
+
+    public By getByXpath(String locator) {
+        return By.xpath(locator);
+    }
+
+    public void clickToElement(WebDriver driver, String locator) {
+        getElement(driver, locator).click();
+    }
+
+    public void sendkeyToElement(WebDriver driver, String locator, String keysToSend) {
+        getElement(driver, locator).sendKeys(keysToSend);
+    }
+
+    public void selectItemInDropdown(WebDriver driver, String locator, String textItem) {
+        new Select(getElement(driver, locator)).selectByVisibleText(textItem);
+    }
+
+    public String getSelectedItemInDropdown(WebDriver driver, String locator) {
+        return new Select(getElement(driver, locator)).getFirstSelectedOption().getText();
+    }
+
+    public boolean isDropdownMultiple(WebDriver driver, String locator) {
+        return new Select(getElement(driver, locator)).isMultiple();
+    }
+
+    public void selectItemInCustomDropdown(WebDriver driver, String parentLocator, String childItemLocator, String expectedItem) {
+        getElement(driver,parentLocator).click();
+        sleepInSeconds(2);
+
+        List<WebElement> allItems = new WebDriverWait(driver, Duration.ofSeconds(15)).
+                until(ExpectedConditions.presenceOfAllElementsLocatedBy(getByXpath(childItemLocator)));
+        sleepInSeconds(2);
+
+        for (WebElement item : allItems){
+            if (item.getText().trim().equals(expectedItem)){
+                item.click();
+                break;
+            }
+        }
+
+    }
+
+    public void sleepInSeconds(long timeInSeconds) {
+        try {
+            Thread.sleep(timeInSeconds * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
