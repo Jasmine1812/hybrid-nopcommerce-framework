8c997e218b7d64c56ff66b458e6406dc247e1914
Admin <jamesnguyen0102@gmail.com>
2024-05-30 04:41:04 +0700

    Java Basic topic 8

--- javaSDET/javaBasic/Topic_05_Loop_Statement.java
+++ javaSDET/javaBasic/Topic_05_Loop_Statement.java
@@ -7,20 +7,39 @@ public class Topic_05_Loop_Statement {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //Bai 7
-//        int n = scanner.nextInt();
-//        for (int i = 0; i < n; i++) {
-//            i == i+ *i;
-//        }
 
-        //Bai 6
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        for (int i = a; i <= b; i++){
-            if (i%3 == 0){
-                System.out.print(i);
-            }
+        int n = scanner.nextInt();
+//        int i = 1;
+//        while (n > 0) {
+//            i *= n;
+//            n--;
+//        }
+        for (int i =1; n > 0; n --){
+            i *= n;
+
+            System.out.println(i);
         }
+
+//        int m = scanner.nextInt();
+//        while (n < m) {
+//            if (n % 3 == 0 && n % 5 == 0) {
+//                System.out.println(n);
+//            }
+//            n++;
+//        }
+//        while (n <= 100){
+//            if (n%2 == 0){
+//                System.out.println(n);
+//            }
+//            n++;
+//        }
+//        do {
+//            if (n % 2 == 0) {
+//                System.out.println(n);
+//            }
+//            n++;
+//        } while (n <= 100);
+
 //        Bai 1
 //      int n = scanner.nextInt();
 //      for (int i = 0; i <= n; i++){
---
+++ javaSDET/javaBasic/Topic_08_String.java
@@ -0,0 +1,78 @@
+package javaBasic;
+
+import org.testng.annotations.Test;
+
+public class Topic_08_String {
+
+    @Test
+    public void TC_01() {
+        String studentName = new String("Nguyen Thi Tuoi 10/04/1991");
+        char newStudentName[] = studentName.toCharArray();
+        int count = 0;
+        for (char i : newStudentName) {
+            if (i >= 'A' && i <= 'Z'){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        int countLow = 0;
+        for (char i : newStudentName) {
+            if (i >= 'a' && i <= 'z'){
+                countLow++;
+            }
+        }
+        System.out.println(countLow);
+
+        int countNumber = 0;
+        for (char i : newStudentName) {
+            if (i >= '0' && i <= '9'){
+                countNumber++;
+            }
+        }
+        System.out.println(countNumber);
+//        String accountName = "Jasmine Nguyen 11 22 33 44";
+//        int number = 1122;
+//        String stringNumber = String.valueOf(number);
+//        System.out.println(stringNumber);
+//
+//        String a = "112";
+//        int newA = Integer.parseInt(a);
+
+//        System.out.println(studenName.trim());
+//        String[] newStudent = studenName.split(" ");
+//        System.out.println(newStudent[0]);
+//        int index = studenName.indexOf("Th");
+//        System.out.println(index);
+//        String sub = studenName.substring(7,12);
+//        System.out.println(sub);
+//        String newString = studenName.replace(" Thi ", " Hong ");
+//        System.out.println(newString);
+//        System.out.println("Do dai chuoi: " + studenName.length());
+//        char partAccountName = accountName.charAt(3);
+//        System.out.println(partAccountName);
+//        System.out.println("Noi chuoi: " + accountName.concat(studenName));
+//        System.out.println(studenName.equals(accountName));
+//        System.out.println(studenName.equalsIgnoreCase(accountName));
+//        System.out.println(studenName.startsWith("Ng"));
+//        System.out.println(studenName.endsWith("ab"));
+//        System.out.println(studenName.contains("Tu"));
+
+
+    }
+
+    @Test
+    public void TC_02(){
+        String string = "Automation Testing 345 Tutorials Online 789";
+        char newChar[] = string.toCharArray();
+        int countA = 0;
+        for (char i : newChar){
+            if (i == 'a'){
+                countA++;
+            }
+        }
+        System.out.println(countA);
+        System.out.println(string.contains("Testing"));
+
+    }
+}
